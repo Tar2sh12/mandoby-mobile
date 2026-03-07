b630b8853c1feb8c63d661d460656a8ef29fb7e0
mohamed tarek <mohamad1tarek1@gmail.com>
2026-03-07 16:04:18 +0200

    feat: add credit and expenses fields to PersonModel and update fromJson method

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -61,13 +61,27 @@ class ShiftModel {
 class PersonModel {
   final String id;
   final String name;
+  final int credit;
+  final int expenses;
   final String? title;
 
-  PersonModel({required this.id, required this.name, this.title});
+  PersonModel({
+    required this.id,
+    required this.name,
+    required this.credit,
+    required this.expenses,
+    this.title,
+  });
 
   factory PersonModel.fromJson(Map<String, dynamic> j) => PersonModel(
     id: j['_id'] ?? j['id'] ?? '',
     name: j['name'] ?? '',
+    credit: (j['credit'] ?? 0) is int
+        ? j['credit']
+        : (j['credit'] as num).toInt(),
+    expenses: (j['expenses'] ?? 0) is int
+        ? j['expenses']
+        : (j['expenses'] as num).toInt(),
     title: j['title'],
   );
 
@@ -127,7 +141,7 @@ class ItemModel {
 
   double get total => cost * quantity;
 
-  //checked setter 
+  //checked setter
   bool settedChecked(bool value) {
     this.checked = value;
     return this.checked;
